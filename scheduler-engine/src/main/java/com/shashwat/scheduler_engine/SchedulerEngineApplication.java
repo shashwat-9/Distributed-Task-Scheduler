95b51a378efa5da7d5fa5d2a60c530be73d211df
Shashwat Mishra <shashwatmishra91@gmail.com>
2025-07-14 22:09:18 +0530

    Scheduler-Engine: Added Initial classes

--- scheduler-engine/src/main/java/com/shashwat/scheduler_engine/SchedulerEngineApplication.java
+++ scheduler-engine/src/main/java/com/shashwat/scheduler_engine/SchedulerEngineApplication.java
@@ -2,12 +2,20 @@ package com.shashwat.scheduler_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulerEngineApplication {
 
+	private static ConfigurableApplicationContext appContext;
+
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulerEngineApplication.class, args);
+		appContext = SpringApplication.run(SchedulerEngineApplication.class, args);
 	}
 
+	public static ConfigurableApplicationContext getAppContext() {
+		return appContext;
+	}
 }
